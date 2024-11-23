404a4730212cc21abafb940ce54ce6bea1023566
Anthony <anthonymwakulila@gmail.com>
2024-11-23 18:15:31 +0300

    added ssl for django

--- app/app/settings.py
+++ app/app/settings.py
@@ -177,4 +177,5 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     'https://recipe-app-react-pi.vercel.app',
 ]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
